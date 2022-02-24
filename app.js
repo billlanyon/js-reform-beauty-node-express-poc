ed8fcc5963eb4117c4349025e7f9f1adc9dbb414
billlanyon <billlanyon@me.com>
2022-02-24 11:59:13 +0100

    adds sessions and CSRF tokens

--- app.js
+++ app.js
@@ -1,25 +1,39 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
+const express = require('express');
+const csrf = require('csurf');
+const expressSession = require('express-session');
 
-const db = require("./data/database");
-const authRoutes = require("./routes/auth.routes");
+const createSessionConfig = require('./config/session');
+const db = require('./data/database');
+const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
+const errorHandlerMiddleware = require('./middlewares/error-handler');
+const authRoutes = require('./routes/auth.routes');
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
+
+const sessionConfig = createSessionConfig()
+
+app.use(expressSession(sessionConfig))
+app.use(csrf());
+
+app.use(addCsrfTokenMiddleware);
 
 app.use(authRoutes);
 
+app.use(errorHandlerMiddleware);
+
 db.connectToDatabase()
-  .then(function () {
-    app.listen(4000);
-  })
-  .catch(function (error) {
-    console.log("Failed to connect to the database!");
-    console.log(error);
-  });
+	.then(function () {
+		app.listen(4000);
+	})
+	.catch(function (error) {
+		console.log('Failed to connect to the database!');
+		console.log(error);
+	});
