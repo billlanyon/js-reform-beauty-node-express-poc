34888094476637a6428fa2da94d522b2631796e8
billlanyon <billlanyon@me.com>
2022-03-02 19:33:11 +0100

    adds product model and outputting product items

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,14 +1,31 @@
-function getProducts(req, res) {
-	res.render('admin/products/all-products');
-}
+const Product = require('../models/product.model');
+
+async function getProducts(req, res, next) {
+	try {
+	  const products = await Product.findAll();
+	  res.render('admin/products/all-products', { products: products });
+	} catch (error) {
+	  next(error);
+	  return;
+	}
+  }
 
 function getNewProduct(req, res) {
 	res.render('admin/products/new-product');
 }
 
-function createNewProduct(req, res) {
-	console.log(req.body);
-	console.log(req.file);
+async function createNewProduct(req, res, next) {
+	const product = new Product({
+		...req.body,
+		image: req.file.filename,
+	});
+
+	try {
+		await product.save();
+	} catch (error) {
+		next(error);
+		return;
+	}
 
 	res.redirect('/admin/products');
 }
