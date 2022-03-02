703da49abb052002da0bf171c3259e5569264de1
billlanyon <billlanyon@me.com>
2022-03-02 11:59:57 +0100

    adds image upload

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,15 +1,20 @@
 function getProducts(req, res) {
-    res.render('admin/products/all-products')
+	res.render('admin/products/all-products');
 }
 
 function getNewProduct(req, res) {
-    res.render('admin/products/new-product')
+	res.render('admin/products/new-product');
 }
 
-function createNewProduct() {}
+function createNewProduct(req, res) {
+	console.log(req.body);
+	console.log(req.file);
+
+	res.redirect('/admin/products');
+}
 
 module.exports = {
-    getProducts: getProducts,
-    getNewProduct: getNewProduct,
-    createNewProduct: createNewProduct,
-}
+	getProducts: getProducts,
+	getNewProduct: getNewProduct,
+	createNewProduct: createNewProduct,
+};
--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,4 +1,5 @@
 const adminController = require('../controllers/admin.controller');
+const imageUploadMiddleware = require('../middlewares/image-upload')
 
 const express = require('express');
 
@@ -7,5 +8,7 @@ const router = express.Router();
 router.get('/products', adminController.getProducts);
 
 router.get('/products/new', adminController.getNewProduct);
+
+router.post('/products', imageUploadMiddleware, adminController.createNewProduct)
 
 module.exports = router;
