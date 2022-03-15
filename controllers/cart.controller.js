42e4339d6fe902ffcecc37339becb7e9c876fd6d
billlanyon <billlanyon@me.com>
2022-03-15 10:14:05 +0100

    adds cart page and updating cart via Ajax requests and updating DOM

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,6 +1,10 @@
 const Product = require('../models/product.model');
 
-async function addCartItem(req, res) {
+function getCart(req, res) {
+	res.render('customer/cart/cart');
+}
+
+async function addCartItem(req, res, next) {
 	let product;
 	try {
 		product = await Product.findById(req.body.productId);
@@ -8,17 +12,40 @@ async function addCartItem(req, res) {
 		next(error);
 		return;
 	}
-	const cart = res.locals.cart
-    
-    cart.addItem(product);
+
+	const cart = res.locals.cart;
+
+	cart.addItem(product);
 	req.session.cart = cart;
 
-    res.status(201).json({
-        message: 'Cart updated',
-        newTotalItems: cart.totalQuantity
-    });
+	res.status(201).json({
+		message: 'Cart updated!',
+		newTotalItems: cart.totalQuantity,
+	});
+}
+
+function updateCartItem(req, res) {
+	const cart = res.locals.cart;
+
+	const updatedItemData = cart.updateItem(
+		req.body.productId,
+		req.body.quantity
+	);
+
+	req.session.cart = cart;
+
+	res.json({
+		message: 'Item updated!',
+		updatedCartData: {
+			newTotalQuantity: cart.totalQuantity,
+			newTotalPrice: cart.totalPrice,
+			updatedItemPrice: updatedItemData.updatedItemPrice,
+		},
+	});
 }
 
 module.exports = {
+	getCart: getCart,
 	addCartItem: addCartItem,
+	updateCartItem: updateCartItem,
 };
--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -4,6 +4,10 @@ const cartController = require('../controllers/cart.controller');
 
 const router = express.Router();
 
+router.get('/', cartController.getCart);
+
 router.post('/items', cartController.addCartItem);
+
+router.patch('/items', cartController.updateCartItem)
 
 module.exports = router;
